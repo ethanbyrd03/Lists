d519e04b6120a15dc7dfb3af4937c3e7182bf56d
Ethan Byrd <ethbyr@ad.unc.edu>
2023-02-15 21:23:07 -0500

    i think so

--- assn2/ArrayList.java
+++ assn2/ArrayList.java
@@ -60,7 +60,7 @@ public class ArrayList implements List {
       if(this.elts[0] > elt) {ins(0, elt); return true;}
       for(int i=0; i<=this.size; i++)
           if(this.elts[i] <= elt && this.elts[i + 1] > elt) {
-              ins(i, elt); return true;
+              ins(i + 1, elt); return true;
           }
       ins(this.size-1, elt); return true;
       }
